eaa9d818e26a5882fcbc296b9f3c5dd788f4c60a
braedonb98 <brbe3574@colorado.edu>
2022-05-20 13:16:57 -0600

    changed general folder structure, working on setting up general folder structure and project to allow for faster creation of future projects by having basic starting "tool kit" still need to update paths

---
+++ schedulebell-F/src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; //also import Navigate for default routing
+import { AuthContext } from "./shared/context/auth-context";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
