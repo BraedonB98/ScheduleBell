8b6b7821588d8fea6c79d49803f7d816b9cb001f
braedonb98 <brbe3574@colorado.edu>
2022-05-25 22:57:27 -0600

    Changes to image storage locations, adding routes for user and location

---
+++ BackEnd/routes/location-routes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const locationController = require("../controllers/location-controller");
+//const fileUpload = require("../middleware/file-upload");
+const checkAuth = require("../middleware/check-auth");
+
+const router = express.Router();
+
+router.use(checkAuth); // every route after this requires an token
+
+router.post("/add", locationController.add);
+
+router.patch("/general", locationController.editGeneral); //including notes
+
+router.patch("/image/:lid", locationController.patchImage);
+
+router.delete("/remove/:lid", locationController.delete);
+
+router.get("/general/:lid", locationController.getGeneral); //name, organization, location number, imageUrl, location
+
+router.get("/notes/:lid", locationController.getNotes);
+
+router.get("/staff/:lid", locationController.getStaff); //Gets active staff
+
+router.get("/staffArchived/:lid", locationController.getArchivedStaff);
+
+router.get("/schedule/:lid", locationController.getActiveSchedule); //Gets active schedule
+
+router.get("/archivedSchedule/:lid", locationController.getLocationStaff);
+
+module.exports = router;
---
+++ BackEnd/routes/user-routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const userController = require("../controllers/user-controller");
+//const fileUpload = require("../middleware/file-upload");
+const checkAuth = require("../middleware/check-auth");
+
+const router = express.Router();
+
+router.post("/login", userController.login);
+
+router.use(checkAuth); // every route after this requires an token
+
+router.post("/addUser", userController.addUser); //Will create a "user" based on username, will set password to LastName, FirstName, no spaces until they sign in the first time
+
+module.exports = router;
