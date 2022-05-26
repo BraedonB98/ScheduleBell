b8fa56da6ba17aaa2158dc4a9b94a40b9911aefe
braedonb98 <brbe3574@colorado.edu>
2022-05-26 00:20:58 -0600

    adding organization controller with empty functions

---
+++ BackEnd/controllers/organization-controller.js
@@ -0,0 +1,30 @@
+//--------------------imports-------------------------
+const mongoose = require("mongoose");
+
+//------------------Modules--------------------------
+
+//------------------Models------------------------------
+const HttpError = require("../models/http-error");
+const Organization = require("../models/organization-model");
+const Location = require("../models/location-model");
+const User = require("../models/user-model");
+//----------------------Controllers-------------------------
+const createOrganization = async (req, res, next) => {};
+const editOrganizationGeneral = async (req, res, next) => {};
+const editOrganizationAccountType = async (req, res, next) => {};
+const editOrganizationAuthorizedUsers = async (req, res, next) => {};
+const patchOrganizationImage = async (req, res, next) => {};
+const deleteOrganization = async (req, res, next) => {};
+const getOrganizationGeneral = async (req, res, next) => {};
+const getOrganizationAccountType = async (req, res, next) => {};
+const getOrganizationLocations = async (req, res, next) => {};
+//---------------------Exports------------------------------
+exports.create = createOrganization;
+exports.editGeneral = editOrganizationGeneral;
+exports.editAccountType = editOrganizationAccountType; //!set timeout 1 month(rerun payment if fail cancel, else reset timeout)
+exports.editAuthorizedUsers = editOrganizationAuthorizedUsers;
+exports.patchImage = patchOrganizationImage;
+exports.delete = deleteOrganization;
+exports.getGeneral = getOrganizationGeneral;
+exports.getAccountType = getOrganizationAccountType;
+exports.getLocations = getOrganizationLocations;
