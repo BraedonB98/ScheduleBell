8e100a002ff812271751e11ba813dd7bdaecc105
braedonb98 <brbe3574@colorado.edu>
2022-05-25 23:09:24 -0600

    adding organization routes

---
+++ BackEnd/routes/organization-routes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const organizationController = require("../controllers/organization-controller");
+//const fileUpload = require("../middleware/file-upload");
+const checkAuth = require("../middleware/check-auth");
+
+const router = express.Router();
+
+router.use(checkAuth); // every route after this requires an token
+
+router.post("/create", organizationController.create);
+
+router.patch("/general/:oid", organizationController.editGeneral); //name, image, colorScheme
+
+router.patch("/accountType/:oid", organizationController.editAccountType); //upgrade subscription
+
+router.patch("/admins/:oid", organizationController.editAuthorizedUsers); //add or remove admins (email both removed and added)
+
+router.patch("/image/:oid", organizationController.patchImage);
+
+router.delete("/remove/:oid", organizationController.delete); //!have a timeout for a 7 days before it actually removes and email owner
+
+router.get("/general/:oid", organizationController.getGeneral);
+
+router.get("/accountType/:oid", organizationController.getAccountType);
+
+router.get("/locations/:oid", organizationController.getLocations);
+
+module.exports = router;
