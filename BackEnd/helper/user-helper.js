9d182d2d4c7532994821f9c169305ebe66983f1a
braedonb98 <brbe3574@colorado.edu>
2022-05-31 18:31:32 -0600

    adding get user helper function also testing if I can just use mongoose and not require(mongodb)

--- BackEnd/helper/user-helper.js
+++ BackEnd/helper/user-helper.js
@@ -1,3 +1,65 @@
+//--------------------Imports-----------------
+const mongoose = require("mongoose");
+const HttpError = require("../models/http-error");
+const User = require("../models/user-model");
+
+//-------------------Functions-----------------
+
+//getUser takes in an identifier and searchType and if there is a user in the database with
+//a user that fits those it will return the user
+const getUser = (identifier, searchType) => {
+  getUserByEN = async (EN) => {
+    let user;
+    if (EN === null) {
+      return new HttpError("no employee number provided", 400);
+    }
+    try {
+      user = await User.findOne({ employeeId: EN });
+    } catch (error) {
+      return new HttpError("Could not access database", 500);
+    }
+    if (!user) {
+      return new HttpError("User not in database", 404);
+    }
+    return user;
+  };
+  const getUserById = async (uid) => {
+    let user;
+    if (uid === null) {
+      return new HttpError("no uid provided", 400);
+    }
+    if (typeof uid === "string") {
+      //!var ObjectID = require("mongodb").ObjectID;
+      uid = new mongoose.ObjectID(uid); //!no mongoose.
+    }
+    try {
+      user = await User.findById(uid);
+    } catch (error) {
+      return {
+        error: error,
+        errorMessage: "Could not access user in database",
+        errorCode: 500,
+      };
+    }
+    if (!user) {
+      return {
+        error: true,
+        errorMessage: "User not in database",
+        errorCode: 404,
+      };
+    }
+    return user;
+  };
+
+  if (searchType === "employeeNumber") {
+    return getUserByEN(identifier);
+  } else if (searchType === "id") {
+    return getUserById(identifier);
+  }
+};
+
+//Restrict User takes in a user to restrict and the level of restiction to apply
+//Then returns a user object with different amounts of information depending on level
 const restrictUser = (user, restrictionLevel) => {
   let userRestricted;
   if (restrictionLevel === "generalEmployees") {
@@ -88,5 +150,7 @@ const restrictUser = (user, restrictionLevel) => {
   }
   return userRestricted;
 };
+
+//-------------Exports--------------
 
 exports.restrictUser = restrictUser;
