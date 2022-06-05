982353f817e6c11ea9305a7971c9d755e3d1801c
braedonb98 <brbe3574@colorado.edu>
2022-06-04 19:07:24 -0600

    made check auth level that does a combination check of permissions and returns http error if not permitted

--- BackEnd/validation/userPermissionValidation.js
+++ BackEnd/validation/userPermissionValidation.js
@@ -6,13 +6,11 @@ const {
 const HttpError = require("../models/http-error");
 const User = require("../models/user-model");
 
-//!this still has alot of work to be done, currently just returning true to all checks to allow me to test code
 //------------------Classes--------------------
 class userPermissionValidation {
   user;
   organization;
   location;
-  users = [];
 
   constructor(user, location) {
     //only requires user but saves from having to get from database if provided by outside function(already queried)
@@ -21,49 +19,58 @@ class userPermissionValidation {
       this.location = location;
     }
   }
-
-  //----Permission Checks--------
-  newLocation() {
-    //check if user is authorized user of organization, if so return false, else return error
-    return false;
-  }
-  newUser() {
-    //check if user is authorized user of location, if so return false else return error
-    // if (not allowed to add user) {
-    //   new HttpError(
-    //     "You dont have required authorization to add a user to this location",
-    //     403
-    //   );
-    // }
-    return false;
-  }
-  organizationPatch() {
-    // if (not allowed authorized user on organization) {
-    //   new HttpError(
-    //     "You dont have required authorization to change organization",
-    //     403
-    //   );
-    // }
-    return false;
-  }
-  getOrganization(minAuth) {
-    //if user does not meet minAuth then it will return an HTTPERROR
-    // if(organizationAdmin)
-    // {
-    //   return "accountAdmin";
-    // }
-    // else if(authorizedUser on organization)
-    // {
-    //   return "authorizedUser";
-    // }
-    // else if(employeeOrganization === organization)
-    // {
-    //   return "generalEmployee";
-    // }
-    // else{
-    //   return ("public")
-    // }
-    return "accountAdmin";
+  checkAuthLevel(minAuth) {
+    //translating min auth to number
+    switch (minAuth) {
+      case "public":
+        minAuth = 0;
+        break;
+      case "general employee":
+        minAuth = 1;
+        break;
+      case "locationAuthUser":
+        minAuth = 2;
+        break;
+      case "organizationAuthUser":
+        minAuth = 3;
+        break;
+      case "accountAdmin":
+        minAuth = 4;
+        break;
+      default:
+        console.log("not valid min Auth in UPV");
+        return new HttpError("unable to figure out min auth in UPV", 500);
+    }
+    //checking users auth and if its above the min auth level
+    if (this.user === this.organization.accountAdmin && minAuth >= 4) {
+      return "accountAdmin";
+    } else if (
+      this.organization.authorizedUsers.includes(this.user) &&
+      minAuth >= 3
+    ) {
+      return "organizationAuthUser";
+    } else if (
+      this.location.authorizedUsers.includes(this.user) &&
+      minAuth >= 2
+    ) {
+      //eventually maybe go through each location in user list
+      return "locationAuthUser";
+    } else if (
+      this.user.organization === organization &&
+      this.location.activeStaff.includes(this.user) &&
+      this.location.organization === this.organization &&
+      minAuth >= 1
+    ) {
+      return "generalEmployee";
+    } else if (minAuth >= 0) {
+      return "public";
+    } //if user does not meet the min auth
+    else {
+      return new HttpError(
+        "You are not authorized to perform that action",
+        403
+      );
+    }
   }
 }
 
