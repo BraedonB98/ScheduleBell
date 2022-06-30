83855ffd8e3ea05f743491496de6c563010c9190
braedonb98 <brbe3574@colorado.edu>
2022-06-29 21:22:58 -0600

    wrapping up nav for tonight because i need to step away for a bit. but I have code for both nav item before and after trying to combine user menu and other nav links into 1 item

--- FrontEnd/src/shared/components/Navigation/elements/NavItem.js
+++ FrontEnd/src/shared/components/Navigation/elements/NavItem.js
@@ -27,7 +27,15 @@ const NavItem = (props) => {
             className="Nav-Item__Button"
             onClick={(event) => props.onOpen(event)}
           >
-            <img alt="User-Menu" src={props.icon} className="Nav-Item__image" />
+            {props.icon ? (
+              <div className="Nav-Item__Button">
+                <img
+                  alt={props.iconAlt}
+                  src={props.icon}
+                  className="Nav-Item__image"
+                />
+              </div>
+            ) : undefined}
           </div>
           {props.children}
         </React.Fragment>
@@ -37,3 +45,34 @@ const NavItem = (props) => {
 };
 
 export default NavItem;
+
+// import React from "react";
+// import { NavLink } from "react-router-dom";
+
+// import "../styling/NavItem.css";
+
+// const NavItem = (props) => {
+//   return (
+//     <li>
+//       <NavLink to={props.to}>
+//         <React.Fragment>
+//           {({ isActive }) =>
+//             isActive ? (
+//               <React.Fragment>
+//                 {props.children ? props.children : ""}
+//               </React.Fragment>
+//             ) : undefined
+//           }
+//           {props.title}
+//           {props.icon ? (
+//             <div className="Nav-Item__Button">
+//               <img alt="NavItem" src={props.icon} className="Nav-Item__image" />
+//             </div>
+//           ) : undefined}
+//         </React.Fragment>
+//       </NavLink>
+//     </li>
+//   );
+// };
+
+// export default NavItem;
