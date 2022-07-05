068db043e39f627afd40da8c3c875e47ea58c238
braedonb98 <brbe3574@colorado.edu>
2022-07-05 07:58:05 -0600

    Adding folders to store test json based on type, also changed organization model color scheme to match the format of app.css

--- BackEnd/models/organization-model.js
+++ BackEnd/models/organization-model.js
@@ -10,7 +10,23 @@ const organizationSchema = new Schema({
   accountType: { type: String }, //Trial, (then subscription options), DevApproved(free but full feature)
   notes: { type: String },
   locations: [{ type: mongoose.Types.ObjectId, ref: "Location" }],
-  organizationColorScheme: [{ type: String }],
+  organizationColorScheme: {
+    colorPrimary: { type: String },
+    colorPrimaryVariant: { type: String },
+    colorSecondary: { type: String },
+    colorSecondaryVariant: { type: String },
+    colorBackground: { type: String },
+    colorSurface: { type: String },
+    colorError: { type: String },
+    colorOnImages: { type: String },
+    colorOnPrimary: { type: String },
+    colorOnSecondary: { type: String },
+    colorOnPrimaryVariant: { type: String },
+    colorOnSecondaryVariant: { type: String },
+    colorOnBackground: { type: String },
+    colorOnSurface: { type: String },
+    colorOnError: { type: String },
+  },
 });
 
 module.exports = mongoose.model("Organization", organizationSchema);
